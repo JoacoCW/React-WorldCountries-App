a0bb0308c7347607d73ee8cb6386538db4113e81
Joaquín Weiss <jweiss2@lenovo.com>
2022-07-28 09:38:19 -0300

    fixed cards not showing

--- src/assets/views/countryCard/countryCard.js
+++ src/assets/views/countryCard/countryCard.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './countryCard.scss';
 
 function countryCard(props) {
-
+    console.log(props);
     return (
         <div className="country-card">
       <div className="country-card__image">
         <img src={props.imgSrc} alt="country-flag"></img>
       </div>
-      <h4>{props.name}</h4>
+      <h4>{props.name.common}</h4>
       <p>{props.population}</p>
 
       <button className="button button--primary" 
